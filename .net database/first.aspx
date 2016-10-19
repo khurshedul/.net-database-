8eb0c4fb8270ae12546c76573cb42e1128f8cd5c
khurshedul barid <khurshedulbarid@gmail.com>
2016-10-19 21:54:23 +0600

    First commit

---
+++ .net database/first.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="first.aspx.cs" Inherits="WebApplication1.first" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+  
+        <asp:MultiView ID="MultiView1" runat="server" ActiveViewIndex="0">
+        
+        <asp:View ID="View1" runat="server">
+                   
+        NAME:
+        <br />
+        <asp:TextBox ID="Name" runat="server"></asp:TextBox>
+            <br />
+        <br />
+        <asp:Button ID="Button2" runat="server"  Text="next" OnClick="Button2_Click" />
+        <br />
+        <br />
+                 </asp:View>
+        <asp:View ID="View2" runat="server">
+             Password:
+        <br />
+        <asp:TextBox ID="Pass" runat="server"></asp:TextBox>
+        <br />
+        Email:<br />
+        <asp:TextBox ID="Email" runat="server" ></asp:TextBox>
+        <br />
+        <asp:Button ID="Button3" runat="server" Text="back" OnClick="Button3_Click" />
+        <asp:Button ID="Button4" runat="server" Text="Confirm" OnClick="Button4_Click" />
+        <br />
+        <br />
+        </asp:View>
+        <asp:View ID="View3" runat="server">
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+            <asp:Button ID="Button5" runat="server" Text="back" OnClick="Button5_Click" />
+        </asp:View>
+  </asp:MultiView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ .net database/first.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class first : System.Web.UI.Page
+    {
+        SqlConnection conn;
+        SqlCommand comm;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DBC"].ConnectionString);
+        }
+
+
+        
+       
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            MultiView1.ActiveViewIndex++;
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            MultiView1.ActiveViewIndex++;
+            string sql= "insert into student values('"+Name.Text+"','"+Email.Text+"','"+Pass.Text+"')";
+            comm = new SqlCommand(sql,conn);
+            conn.Open();
+            comm.ExecuteNonQuery();
+            sql = "select *  from student";
+            comm = new SqlCommand(sql, conn);
+            SqlDataReader da= comm.ExecuteReader();
+            GridView1.DataSource=da;
+            GridView1.DataBind();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            MultiView1.ActiveViewIndex--;
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            MultiView1.ActiveViewIndex--;
+        }
+    }
+}
